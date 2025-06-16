e033f331c55af0bab143908a2eabdbc94b2de511
Dmitry <d@kozlov.ru>
2025-06-16 11:13:38 +0300

    Updated Channel

--- index.ts
+++ index.ts
@@ -5,7 +5,23 @@ interface Login {
   token: string
   app: string
 }
-new Service()
+let address: string | undefined
+const argv = process.argv.slice(2)
+while (true) {
+  const command = argv.shift()
+  if (!command) break
+  switch (command) {
+    case '-p':
+    case '--port':
+      const port = Number(argv.shift())
+      if (!isNaN(port)) address = `ws://localhost:${port}`
+      break
+    default:
+      break
+  }
+}
+
+new Service(address)
   .post('apple/login', (body: Login) => verify(body.token, body.app))
   .post('apple/login/lite', (body: Login) => verify(body.token, body.app).then(a => a?.sub))
   .post('apple/storekit2', decode)
